c8e35b3f4a456c3ba7e5bdc4551c8dc3b7c8adb6
Vinson7lhl <vinson7lhl@126.com>
2020-08-03 16:24:17 +0800

    优化ElementUI“树代码
    优化ElementUI“树代码”

--- vue_app/src/views/ExportExl.vue
+++ vue_app/src/views/ExportExl.vue
@@ -1,44 +1,41 @@
 <template>
-	<div class="exportExl">
-		<table border="1">
-			<tr>
-				<th rowspan="2">F</th>
-				<th>G</th>
-				<th colspan="3">A</th>
-			</tr>
-		</table>
-		
-		
-		导出excel文件！左侧树结构，右侧容器table
-		<el-tree
-			draggable
-			:data="data"
-			:props="defaultProps"
-			:allow-drop="allowDrop"
-			default-expand-all
-			@node-click="handleNodeClick"
-			@node-drop="handleDrop"
-			@check="getTreeData"
-			ref="thisTree"
-		></el-tree>
-		<el-table :data="tableData" style="width: 100%">
-			<el-table-column prop="date" label="日期" width="150"></el-table-column>
-			<el-table-column label="配送信息">
-				<el-table-column prop="name" label="姓名" width="120"></el-table-column>
-				<el-table-column label="地址">
-					<el-table-column prop="province" label="省份" width="120"></el-table-column>
-					<el-table-column prop="city" label="市区" width="120"></el-table-column>
-					<el-table-column prop="address" label="地址" width="300"></el-table-column>
-					<el-table-column prop="zip" label="邮编" width="120"></el-table-column>
-				</el-table-column>
-			</el-table-column>
-		</el-table>
-	</div>
+  <div class="exportExl">
+    <table border="1">
+      <tr>
+        <th rowspan="2">F</th>
+        <th>G</th>
+        <th colspan="3">A</th>
+      </tr>
+    </table>导出excel文件！左侧树结构，右侧容器table
+    <el-tree
+      draggable
+      :data="data"
+      :props="defaultProps"
+      :allow-drop="allowDrop"
+      default-expand-all
+      @node-click="handleNodeClick"
+      @node-drop="handleDrop"
+      @check="getTreeData"
+      ref="thisTree"
+    ></el-tree>
+    <el-table :data="tableData" style="width: 100%">
+      <el-table-column prop="date" label="日期" width="150"></el-table-column>
+      <el-table-column label="配送信息">
+        <el-table-column prop="name" label="姓名" width="120"></el-table-column>
+        <el-table-column label="地址">
+          <el-table-column prop="province" label="省份" width="120"></el-table-column>
+          <el-table-column prop="city" label="市区" width="120"></el-table-column>
+          <el-table-column prop="address" label="地址" width="300"></el-table-column>
+          <el-table-column prop="zip" label="邮编" width="120"></el-table-column>
+        </el-table-column>
+      </el-table-column>
+    </el-table>
+  </div>
 </template>
 
 <script>
 	export default {
-		name: "about-page",
+		name: 'about-page',
 		data() {
 			return {
 				// 树结构元数据
@@ -47,56 +44,56 @@
 				// 其它字段可自行定义
 				data: [
 					{
-						label: "行",
-						level: "top",
-						type_name: "row",
+						label: '行',
+						level: 'top',
+						type_name: 'row',
 						id: 1,
 					},
 					{
-						label: "列",
-						level: "top",
-						type_name: "col",
+						label: '列',
+						level: 'top',
+						type_name: 'col',
 						id: 2,
 					},
 					{
-						label: "类型",
-						level: "top",
-						type_name: "type",
+						label: '类型',
+						level: 'top',
+						type_name: 'type',
 						id: 3,
 						children: [
 							{
-								label: "姓名",
-								id: 3,
-								level: "son",
-								type_name: "name",
+								label: '姓名',
+								id: 8,
+								level: 'son',
+								type_name: 'name',
 								is_in_type: true,
 							},
 							{
-								label: "年龄",
+								label: '年龄',
 								id: 4,
-								level: "son",
-								type_name: "age",
+								level: 'son',
+								type_name: 'age',
 								is_in_type: true,
 							},
 							{
-								label: "周期",
+								label: '周期',
 								id: 5,
-								level: "son",
-								type_name: "period",
+								level: 'son',
+								type_name: 'period',
 								is_in_type: true,
 							},
 							{
-								label: "价格",
+								label: '价格',
 								id: 6,
-								level: "son",
-								type_name: "price",
+								level: 'son',
+								type_name: 'price',
 								is_in_type: true,
 							},
 							{
-								label: "体积",
+								label: '体积',
 								id: 7,
-								level: "son",
-								type_name: "volume",
+								level: 'son',
+								type_name: 'volume',
 								is_in_type: true,
 							},
 						],
@@ -105,76 +102,76 @@
 				// 生成的table每行title数据
 				table_row_head_data: [],
 				// 生成的table每列title数据
-				table_colum_head_data: ["类型"],
+				table_colum_head_data: ['类型'],
 
 				defaultProps: {
-					children: "children",
-					label: "label",
+					children: 'children',
+					label: 'label',
 				},
 				tableData: [
 					{
-						date: "2016-05-03",
-						name: "王小虎",
-						province: "上海",
-						city: "普陀区",
-						address: "上海市普陀区金沙江路 1518 弄",
+						date: '2016-05-03',
+						name: '王小虎',
+						province: '上海',
+						city: '普陀区',
+						address: '上海市普陀区金沙江路 1518 弄',
 						zip: 200333,
 					},
 					{
-						date: "2016-05-02",
-						name: "王小虎",
-						province: "上海",
-						city: "普陀区",
-						address: "上海市普陀区金沙江路 1518 弄",
+						date: '2016-05-02',
+						name: '王小虎',
+						province: '上海',
+						city: '普陀区',
+						address: '上海市普陀区金沙江路 1518 弄',
 						zip: 200333,
 					},
 					{
-						date: "2016-05-04",
-						name: "王小虎",
-						province: "上海",
-						city: "普陀区",
-						address: "上海市普陀区金沙江路 1518 弄",
+						date: '2016-05-04',
+						name: '王小虎',
+						province: '上海',
+						city: '普陀区',
+						address: '上海市普陀区金沙江路 1518 弄',
 						zip: 200333,
 					},
 					{
-						date: "2016-05-01",
-						name: "王小虎",
-						province: "上海",
-						city: "普陀区",
-						address: "上海市普陀区金沙江路 1518 弄",
+						date: '2016-05-01',
+						name: '王小虎',
+						province: '上海',
+						city: '普陀区',
+						address: '上海市普陀区金沙江路 1518 弄',
 						zip: 200333,
 					},
 					{
-						date: "2016-05-08",
-						name: "王小虎",
-						province: "上海",
-						city: "普陀区",
-						address: "上海市普陀区金沙江路 1518 弄",
+						date: '2016-05-08',
+						name: '王小虎',
+						province: '上海',
+						city: '普陀区',
+						address: '上海市普陀区金沙江路 1518 弄',
 						zip: 200333,
 					},
 					{
-						date: "2016-05-06",
-						name: "王小虎",
-						province: "上海",
-						city: "普陀区",
-						address: "上海市普陀区金沙江路 1518 弄",
+						date: '2016-05-06',
+						name: '王小虎',
+						province: '上海',
+						city: '普陀区',
+						address: '上海市普陀区金沙江路 1518 弄',
 						zip: 200333,
 					},
 					{
-						date: "2016-05-07",
-						name: "王小虎",
-						province: "上海",
-						city: "普陀区",
-						address: "上海市普陀区金沙江路 1518 弄",
+						date: '2016-05-07',
+						name: '王小虎',
+						province: '上海',
+						city: '普陀区',
+						address: '上海市普陀区金沙江路 1518 弄',
 						zip: 200333,
 					},
 				],
-				html_str: "",
-			};
+				html_str: '',
+			}
 		},
 		methods: {
 			handleNodeClick(data) {
-				console.log(data);
+				console.log(data)
 			},
 			// 是否允许放置
 			/**
@@ -189,56 +186,57 @@
 			allowDrop(draggingNode, dropNode, type) {
 				// 如果拖放在行模块
 				if (
-					draggingNode.data.level === "son" &&
-					(dropNode.data.type_name === "row" ||
-						dropNode.data.type_name === "col")
+					draggingNode.data.level === 'son' &&
+					(dropNode.data.type_name === 'row' ||
+						dropNode.data.type_name === 'col')
 				) {
-					return type === "inner";
+					return type === 'inner'
 				} else if (
-					draggingNode.data.level === "son" &&
-					dropNode.data.level === "son" &&
+					draggingNode.data.level === 'son' &&
+					dropNode.data.level === 'son' &&
 					draggingNode.data.is_in_type &&
 					!dropNode.data.is_in_type
 				) {
-					return type === "inner";
+					return type === 'inner'
 				} else if (
-					draggingNode.data.level === "son" &&
-					!draggingNode.data.children &&
-					dropNode.data.type_name === "type"
+					draggingNode.data.level === 'son' &&
+					(!draggingNode.data.children ||
+						draggingNode.data.children.length === 0) &&
+					dropNode.data.type_name === 'type'
 				) {
-					return type === "inner";
+					return type === 'inner'
 				} else if (
-					draggingNode.data.level === "son" &&
-					dropNode.data.level === "son" &&
+					draggingNode.data.level === 'son' &&
+					dropNode.data.level === 'son' &&
 					!draggingNode.data.is_in_type &&
 					!dropNode.data.is_in_type
 				) {
-					return type === "inner";
+					return type === 'inner'
 				} else {
-					return false;
+					return false
 				}
 			},
 			// 放置成功后
 			handleDrop(draggingNode, dropNode, dropType, ev) {
 				// 获取拖拽后的完整数据
-				console.log(this.$refs.thisTree.root.data);
+				console.log(this.$refs.thisTree.root.data)
 				// 如果拖放在行、列模块
 				if (
-					dropNode.data.type_name === "row" ||
-					dropNode.data.type_name === "col" ||
-					dropNode.data.level === "son"
+					dropNode.data.type_name === 'row' ||
+					dropNode.data.type_name === 'col' ||
+					dropNode.data.level === 'son'
 				) {
-					draggingNode.data.is_in_type = false;
-				} else if (dropNode.data.type_name === "type") {
-					draggingNode.data.is_in_type = true;
+					draggingNode.data.is_in_type = false
+				} else if (dropNode.data.type_name === 'type') {
+					draggingNode.data.is_in_type = true
 				}
 			},
 			getTreeData(data, checked, indeterminate) {
-				var data1 = this.$refs.multipleTable.getCheckedNodes();
-				console.log(data1);
+				var data1 = this.$refs.multipleTable.getCheckedNodes()
+				console.log(data1)
 			},
 		},
-	};
+	}
 </script>
 
 <style lang="scss" scoped>
