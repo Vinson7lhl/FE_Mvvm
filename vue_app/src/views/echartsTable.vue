21fb5fcfec247c99f2123def90c47364dc6a2721
Vinson7lhl <vinson7lhl@126.com>
2020-08-10 16:50:08 +0800

    提交

--- vue_app/src/views/echartsTable.vue
+++ vue_app/src/views/echartsTable.vue
@@ -23,21 +23,155 @@
 				let myChart1 = this.$Echarts.init(_this.$refs.echarts_container1)
 				// 绘制图表
 				myChart1.setOption({
+					// 设置全局字体
+					textStyle: {
+						fontFamily: 'Arial',
+					},
 					xAxis: {
 						type: 'category',
 						boundaryGap: false,
-						data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+						data: [
+							'2019-03',
+							'2019-04',
+							'2019-05',
+							'2019-06',
+							'2019-07',
+							'2019-08',
+							'2019-09',
+							'2019-10',
+							'2019-11',
+							'2019-12',
+							'2020-01',
+							'2020-02',
+						],
+						// x坐标轴线是否显示
+						axisLine: {
+							show: false,
+						},
+						// x坐标轴刻度是否显示
+						axisTick: {
+							show: false,
+						},
+						// x轴坐标label文本设置
+						axisLabel: {
+							// 字体大小10
+							fontSize: 10,
+							// 距离顶部20
+							padding: [20, 0, 0, 0],
+							color: '#ffffff',
+						},
 					},
 					yAxis: {
+						// y轴的标题名
+						name: '点数',
+						// y轴的标题名样式，因为要让名字在量度顶部，调整padding-left:-80
+						nameTextStyle: {
+							color: '#ffffff',
+							padding: [0, 0, 0, -80],
+						},
 						type: 'value',
+						// y坐标轴线是否显示
+						axisLine: {
+							show: false,
+						},
+						// y坐标轴刻度是否显示
+						axisTick: {
+							show: false,
+						},
+						// y轴坐标label文本设置
+						axisLabel: {
+							// 字体大小10
+							fontSize: 10,
+							// 距离顶部20
+							padding: [0, 20, 0, 0],
+							color: '#ffffff',
+						},
+						// 不要显示y轴的分割线
+						splitLine: {
+							show: false,
+						},
 					},
 					series: [
 						{
-							data: [820, 932, 901, 934, 1290, 1330, 1320],
+							data: [
+								820,
+								932,
+								901,
+								934,
+								1290,
+								1330,
+								1320,
+								223,
+								333,
+								1233,
+								2783,
+								921,
+							],
 							type: 'line',
-							areaStyle: {},
+							// 是否平滑过渡
+							smooth: true,
+							// 打点图形：空心圆
+							symbol: 'emptyCircle',
+							// 打点大小
+							symbolSize: 8,
+							// 平滑曲线的样式
+							lineStyle: {
+								// 线宽
+								width: 3,
+								color: '#A634EF',
+							},
+							// 数据显示区域填充颜色
+							areaStyle: {
+								// 颜色线性渐变
+								color: {
+									// 渐变方式
+									type: 'linear',
+									x: 0,
+									y: 0,
+									x2: 0,
+									y2: 1,
+									colorStops: [
+										{
+											offset: 0,
+											color: 'rgb(224,109,255)',
+										},
+										{
+											offset: 1,
+											color: 'rgb(140,84,255, 0.1)',
+										},
+									],
+									global: false, // 缺省为 false
+								},
+							},
 						},
 					],
+					// 鼠标放上后的数据提示框
+					tooltip: {
+						trigger: 'axis',
+						// 十字标尺
+						axisPointer: {
+							// 不显示十字标尺
+							type: 'none',
+							// 不显示标尺xy轴的label
+							label: {
+								show: false,
+							},
+						},
+						// 提示框内边距 [上 右 下 左]
+						padding: [0, 5, 0, 5],
+						// 背景色
+						backgroundColor: '#A634EF',
+						// 文本样式
+						textStyle: {
+							color: '#ffffff',
+							fontWeight: 'bold',
+							fontSize: 23,
+						},
+						// 数据格式化，此处为 123456 => 123,456
+						formatter: (params) => {
+							return _this.numFormat(params[0].data)
+						},
+					},
 				})
 
 				// 基于准备好的dom，初始化echarts实例
@@ -90,14 +224,25 @@
 					],
 				})
 			},
+			numFormat(num) {
+				var res = num.toString().replace(/\d+/, function (n) {
+					// 先提取整数部分
+					return n.replace(/(\d)(?=(\d{3})+$)/g, function ($1) {
+						return $1 + ','
+					})
+				})
+				return res
+			},
 		},
 		components: {},
 	}
 </script>
 <style lang="scss" scoped>
 	.dashboard1 {
-		width: 300px;
+		width: 760px;
 		height: 300px;
+		background-color: #47059c;
+		border-radius: 14px;
 	}
 	.dashboard2 {
 		width: 300px;
