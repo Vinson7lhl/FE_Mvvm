d34f00147518b2644cc261427e6b3c7714b58522
Seven Li-后会有七 <vinson7lhl@126.com>
2020-08-15 23:31:34 +0800

    提交

--- vue_app/src/api/base_api.js
+++ vue_app/src/api/base_api.js
@@ -8,7 +8,8 @@ const HTTP = axios.create({
 
 // 添加请求拦截器
 HTTP.interceptors.request.use(function (config) {
-	// 在发送请求之前做些什么
+    // 在发送请求之前做些什么，比如登录校验
+    console.log('http-header-config:', config)
 	return config
 }, function (error) {
 	// 对请求错误做些什么
@@ -18,8 +19,6 @@ HTTP.interceptors.request.use(function (config) {
 // 添加响应拦截器
 HTTP.interceptors.response.use(function (response) {
     // 对响应数据做点什么
-    console.log('response 拦截器')
-    console.log(response)
 	return response.data;
 }, function (error) {
 	// 对响应错误做点什么
