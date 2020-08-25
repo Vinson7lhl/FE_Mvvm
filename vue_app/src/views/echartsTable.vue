3358536cbce3e67e05880c9be7ab440c88e35a3c
Vinson7lhl <vinson7lhl@126.com>
2020-08-25 17:10:59 +0800

    提交

--- vue_app/src/views/echartsTable.vue
+++ vue_app/src/views/echartsTable.vue
@@ -9,20 +9,27 @@
 <script>
 	export default {
 		name: "dashboard-page",
-		data () {
+		data() {
 			return {};
 		},
-		mounted () {
+		mounted() {
 			console.log("进入dashboard");
 			this.initCharts();
 		},
 		methods: {
-			async initCharts () {
+			async initCharts() {
 				// 基于准备好的容器1，折线图
 				var _this = this;
 				let myChart1 = this.$Echarts.init(_this.$refs.echarts_container1);
 				// 绘制折线图
 				myChart1.setOption({
+					// 决定折线图在容器内的大小，xy为偏移量
+					grid: {
+						left: "12%", //x 偏移量
+						top: "25%", // y 偏移量
+						width: "82%", // 宽度
+						height: "50%", // 高度
+					},
 					// 设置全局字体
 					textStyle: {
 						fontFamily: "Arial",
@@ -94,57 +101,60 @@
 						},
 					},
 					// 核心数据轴的配置
-					series: [{
-						data: [
-							820,
-							932,
-							901,
-							934,
-							1290,
-							1330,
-							1320,
-							223,
-							333,
-							1233,
-							2783,
-							921,
-						],
-						type: "line",
-						// 是否平滑过渡
-						smooth: true,
-						// 打点图形：空心圆
-						symbol: "emptyCircle",
-						// 打点大小
-						symbolSize: 8,
-						// 平滑曲线的样式
-						lineStyle: {
-							// 线宽
-							width: 3,
-							color: "#A634EF",
-						},
-						// 数据显示区域填充颜色
-						areaStyle: {
-							// 颜色线性渐变
-							color: {
-								// 渐变方式
-								type: "linear",
-								x: 0,
-								y: 0,
-								x2: 0,
-								y2: 1,
-								colorStops: [{
-										offset: 0,
-										color: "rgb(224,109,255)",
-									},
-									{
-										offset: 1,
-										color: "rgb(140,84,255, 0.1)",
-									},
-								],
-								global: false, // 缺省为 false
+					series: [
+						{
+							data: [
+								820,
+								932,
+								901,
+								934,
+								1290,
+								1330,
+								1320,
+								223,
+								333,
+								1233,
+								2783,
+								921,
+							],
+							type: "line",
+							// 是否平滑过渡
+							smooth: true,
+							// 打点图形：空心圆
+							symbol: "emptyCircle",
+							// 打点大小
+							symbolSize: 8,
+							// 平滑曲线的样式
+							lineStyle: {
+								// 线宽
+								width: 3,
+								color: "#A634EF",
+							},
+							// 数据显示区域填充颜色
+							areaStyle: {
+								// 颜色线性渐变
+								color: {
+									// 渐变方式
+									type: "linear",
+									x: 0,
+									y: 0,
+									x2: 0,
+									y2: 1,
+									colorStops: [
+										{
+											offset: 0,
+											color: "rgb(224,109,255)",
+										},
+										{
+											offset: 1,
+											color: "rgba(71,5,156,1)",
+										},
+									],
+									global: false, // 缺省为 false
+								},
 							},
 						},
-					}, ],
+					],
 					// 鼠标放上后的数据提示框
 					tooltip: {
 						trigger: "axis",
@@ -180,25 +190,22 @@
 				// 基于准备好的容器2，雷达图
 				let myChart2 = this.$Echarts.init(_this.$refs.echarts_container2);
 				// 绘制图表
-				let indicator_array = [{
+				let indicator_array = [
+					{
 						name: "苹果",
 						max: 10000,
-						color: "#ffffff",
 					},
 					{
 						name: "鸭梨",
 						max: 10000,
-						color: "#ffffff",
 					},
 					{
 						name: "菠萝",
 						max: 10000,
-						color: "#ffffff",
 					},
 					{
 						name: "葡萄",
 						max: 10000,
-						color: "#ffffff",
 					},
 				];
 				myChart2.setOption({
@@ -243,75 +250,89 @@
 						// 指示器名是否显示（即那几个维度名字）
 						name: {
 							show: true,
+							textStyle: {
+								color: "#000000",
+							},
 						},
 						// 指示器名字离雷达图距离
 						nameGap: 10,
 					},
-					series: [{
-						type: "radar",
-						tooltip: {
-							trigger: "item",
-						},
-						// 打点图形：空心圆
-						symbol: "emptyCircle",
-						// 打点大小
-						symbolSize: 8,
-						label: {
-							show: false,
-						},
-						areaStyle: {},
-						data: [{
-								value: [8260, 7113, 5185, 6310],
-								name: "某软件",
-								itemStyle: {
-									// 雷达图圆点颜色
-									color: "#964FFF",
-								},
-								// 雷达图面积颜色
-								areaStyle: {
-									color: "#964FFF",
-									opacity: 0.8,
-								},
+					series: [
+						{
+							type: "radar",
+							tooltip: {
+								trigger: "item",
 							},
-							{
-								value: [3920, 5553, 7128, 8790],
-								name: "某软件2",
-								itemStyle: {
+							// 打点图形：空心圆
+							symbol: "emptyCircle",
+							// 打点大小
+							symbolSize: 8,
+							label: {
+								show: false,
+							},
+							areaStyle: {},
+							data: [
+								{
+									value: [8260, 7113, 5185, 6310],
+									name: "某软件",
+									itemStyle: {
+										// 雷达图圆点颜色
+										color: "#964FFF",
+									},
 									// 雷达图面积颜色
-									color: "#02C5D7",
+									areaStyle: {
+										color: "#964FFF",
+										opacity: 0.8,
+									},
 								},
-								areaStyle: {
-									color: "#02C5D7",
-									opacity: 0.8,
+								{
+									value: [3920, 5553, 7128, 8790],
+									name: "某软件2",
+									itemStyle: {
+										// 雷达图面积颜色
+										color: "#02C5D7",
+									},
+									areaStyle: {
+										color: "#02C5D7",
+										opacity: 0.8,
+									},
 								},
-							},
-						],
-						// 鼠标放上后的数据提示框
-						tooltip: {
-							// 提示框内边距 [上 右 下 左]
-							padding: [5, 5, 5, 5],
-							// 背景色
-							backgroundColor: "#A634EF",
-							// 文本样式
-							textStyle: {
-								color: "#ffffff",
-								fontWeight: "bold",
-								fontSize: 18,
-							},
-							formatter: (params) => {
-								return `<div>${params.name}</div>
-										<div style="font-size:14px">${indicator_array[0].name} ${_this.numFormat(params.value[0])}</div>
-										<div style="font-size:14px">${indicator_array[1].name} ${_this.numFormat(params.value[1])}</div>
-										<div style="font-size:14px">${indicator_array[2].name} ${_this.numFormat(params.value[2])}</div>
-										<div style="font-size:14px">${indicator_array[3].name} ${_this.numFormat(params.value[3])}</div>`;
+							],
+							// 鼠标放上后的数据提示框
+							tooltip: {
+								// 提示框内边距 [上 右 下 左]
+								padding: [5, 5, 5, 5],
+								// 背景色
+								backgroundColor: "#A634EF",
+								// 文本样式
+								textStyle: {
+									color: "#ffffff",
+									fontWeight: "bold",
+									fontSize: 18,
+								},
+								formatter: (params) => {
+									return `<div>${params.name}</div>
+											<div style="font-size:14px">${indicator_array[0].name} ${_this.numFormat(
+										params.value[0]
+									)}</div>
+											<div style="font-size:14px">${indicator_array[1].name} ${_this.numFormat(
+										params.value[1]
+									)}</div>
+											<div style="font-size:14px">${indicator_array[2].name} ${_this.numFormat(
+										params.value[2]
+									)}</div>
+											<div style="font-size:14px">${indicator_array[3].name} ${_this.numFormat(
+										params.value[3]
+									)}</div>`;
+								},
 							},
 						},
-					}, ],
+					],
 				});
 
 				// 基于准备好的容器3，饼图
 				let myChart3 = this.$Echarts.init(_this.$refs.echarts_container3);
-				console.log('1')
+				console.log("1");
 				// 绘制饼图
 				await myChart3.setOption({
 					tooltip: {
@@ -320,90 +341,97 @@
 						alwaysShowContent: true,
 					},
 					color: ["#19BE6B", "#ED3F14", "#A634EF", "#F8E71C", "#F5A623"],
-					series: [{
-						name: "访问来源",
-						type: "pie",
-						radius: ["40%", "45%"],
-						avoidLabelOverlap: false,
-						// 禁用鼠标触发事件
-						// silent: true,
-						// 鼠标hover时激活大小
-						hoverOffset: 3,
-						// 类型标签样式
-						label: {
-							show: true,
-							position: "outside",
-							fontSize: 12,
-						},
-						// 标签和饼图线样式,首先让其不显示，其次让其第一段第二段引导线变短即可调整label与饼图距离
-						labelLine: {
-							show: false,
-							length: 10,
-							length2: 1,
-						},
-						// 高亮后饼图，和标签样式
-						emphasis: {
+					series: [
+						{
+							name: "访问来源",
+							type: "pie",
+							radius: ["50%", "75%"],
+							avoidLabelOverlap: false,
+							// 禁用鼠标触发事件
+							// silent: true,
+							// 鼠标hover时激活大小
+							hoverOffset: 3,
+							// 类型标签样式
 							label: {
 								show: true,
-								fontSize: "16",
+								position: "outside",
+								fontSize: 12,
 							},
+							// 设置各个子饼图之间距离，
+							itemStyle:{
+								borderWidth:5,	//边框的宽度
+								borderColor:'#47059c'	//边框的颜色
+							},
+							// 标签和饼图线样式,首先让其不显示，其次让其第一段第二段引导线变短即可调整label与饼图距离
+							labelLine: {
+								show: false,
+								length: 10,
+								length2: 1,
+							},
+							// 高亮后饼图，和标签样式
+							emphasis: {
+								label: {
+									show: true,
+									fontSize: "16",
+								},
+							},
+							tooltip: {
+								// 背景色
+								backgroundColor: "transparent",
+								position: ["35%", "40%"],
+								padding: 0,
+								// 文本样式
+								textStyle: {
+									color: "#ffffff",
+									fontWeight: "bold",
+									fontSize: 26,
+								},
+								// formatter: '<div style="text-align:center;line-height:24px">{c}<br/><div style="font-size:14px;font-style:italic">({d})%</div></div>'
+								formatter: function (params) {
+									return (
+										'<div style="text-align:center;width:88px">' +
+										_this.numFormat(params.value) +
+										'</div><div style="font-size:14px;font-style:italic;line-height:14px;text-align:center;width:88px">(' +
+										params.percent +
+										"%)</div>"
+									);
+								},
+							},
+							data: [
+								{ value: 1153, name: "直接访问" },
+								{ value: 1200, name: "邮件营销" },
+								{ value: 1334, name: "联盟广告" },
+								{ value: 1495, name: "视频广告" },
+								{ value: 1548, name: "搜索引擎" },
+							],
 						},
-						tooltip: {
-							// 背景色
-							backgroundColor: "transparent",
-							position: ["35%", "40%"],
-							padding: 0,
-							// 文本样式
-							textStyle: {
-								color: "#ffffff",
-								fontWeight: "bold",
-								fontSize: 26,
-							},
-							// formatter: '<div style="text-align:center;line-height:24px">{c}<br/><div style="font-size:14px;font-style:italic">({d})%</div></div>'
-							formatter: function (params) {
-								return (
-									'<div style="text-align:center;width:88px">' +
-									_this.numFormat(params.value) +
-									'</div><div style="font-size:14px;font-style:italic;line-height:14px;text-align:center;width:88px">(' +
-									params.percent +
-									"%)</div>"
-								);
-							},
-						},
-						data: [
-							{ value: 1153, name: "直接访问" },
-							{ value: 1200, name: "邮件营销" },
-							{ value: 1334, name: "联盟广告" },
-							{ value: 1495, name: "视频广告" },
-							{ value: 1548, name: "搜索引擎" },
-						],
-					}],
+					],
 				});
-				let i = 0
+				let i = 0;
 				setInterval(() => {
 					if (i > 0) {
 						myChart3.dispatchAction({
 							type: "downplay",
 							dataIndex: i - 1,
-						})
+						});
 						if (i === 5) {
-							i = 0
+							i = 0;
 						}
 					}
 					myChart3.dispatchAction({
 						type: "highlight",
 						dataIndex: i,
-					})
+					});
 					myChart3.dispatchAction({
-						type: 'showTip',
+						type: "showTip",
 						dataIndex: i,
 						seriesIndex: 0,
-						position: ['35%', '40%'],
-					})
-					i++
-				}, 2000)
+						position: ["35%", "40%"],
+					});
+					i++;
+				}, 2000);
 			},
-			numFormat (num) {
+			numFormat(num) {
 				var res = num.toString().replace(/\d+/, function (n) {
 					// 先提取整数部分
 					return n.replace(/(\d)(?=(\d{3})+$)/g, function ($1) {
@@ -418,8 +446,8 @@
 </script>
 <style lang="scss" scoped>
 	.dashboard1 {
-		width: 760px;
-		height: 300px;
+		width: 628px;
+		height: 171px;
 		background-color: #47059c;
 		border-radius: 14px;
 	}
@@ -434,8 +462,8 @@
 	}
 
 	.dashboard3 {
-		width: 300px;
-		height: 300px;
+		width: 340px;
+		height: 180px;
 		border-radius: 14px;
 		margin-top: 20px;
 		background-color: #47059c;
