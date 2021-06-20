5019c7c6d40f426093cd9b6e7da86d757b5cbe31
Seven Li-后会有七 <vinson7lhl@126.com>
2021-06-20 23:17:10 +0800

    提交

--- vue_app/src/components/tab1/tab1.vue
+++ vue_app/src/components/tab1/tab1.vue
@@ -5,12 +5,30 @@ export default {
 	// 组件名，其实不写这个属性也可以
 	name: 'Tab1',
 	// 组件上的属性
-	props: {},
+	props: [],
 	/**
 	 * 状态，必须是函数，然后返回状态，而不可以直接映射json对象
 	 */
 	data () {
 		return {}
+	},
+	beforeCreate () {
+		console.log('---:is指令tab1组件：beforeCreate---')
+	},
+	created () {
+		console.log('---:is指令tab1组件:created---')
+	},
+	beforeMount () {
+		console.log('---:is指令tab1组件:beforeMount---')
+	},
+	mounted () {
+		console.log('---:is指令tab1组件：mounted---')
+	},
+	beforeDestroy () {
+		console.log('---:is指令tab1组件：beforeDestroy---')
+	},
+	destroyed () {
+		console.log('---:is指令tab1组件：destroyed---')
 	},
 	/**
 	 * 函数
