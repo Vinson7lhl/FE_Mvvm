777ae13f71e5a2dd45cd0201957ca77e3d5a8aae
Seven Li-后会有七 <vinson7lhl@126.com>
2021-07-08 09:39:16 +0800

    提交：修改eslint规范参数

--- vue3_app/.eslintrc.js
+++ vue3_app/.eslintrc.js
@@ -18,31 +18,31 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    // 函数定义的时候不允许出现重复的参数
+    // 禁止函数定义的时候出现重复的参数
     "no-dupe-args": 2, 
-    // 对象中不允许出现重复的键
+    // 禁止对象中出现重复的键
     "no-dupe-keys": 2,
-    // 不允许出现空的代码块
+    // 禁止出现空的代码块
     "no-empty": 2,
-    // 不允许出现不必要的分号
+    // 禁止出现不必要的分号
     "no-extra-semi": 2,
     // 禁止语句分号结尾
     "semi": [2, "never"],
-     // 分号前不空格，后空格
+     // 分号后前不空格，后空格
     "semi-spacing": [2, {"before": false, "after": true}],
     // 不允许出现多余的空格
     "no-multi-spaces": 2,
-    // 不允许使用tab（关闭）
+    // 允许使用tab
     "no-tabs": 0,
     // 强制一致的缩进风格:tab
     "indent": [2, "tab"],
-    // 不允许使用var
+    // 禁止许使用var
     "no-var": 2, 
-    // 使用单引号
+    // 强制使用单引号
     "quotes": [2, "single"],
     // 不强制驼峰命名规则 
     "camelcase": 0,
-    // 最后一行是否留出空行- 不必
-    "eol-last": 0
+    // 禁止尾随逗号
+    "comma-dangle":  [2, "never"]
   }
 }
