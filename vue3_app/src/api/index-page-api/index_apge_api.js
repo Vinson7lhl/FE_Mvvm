22a771920dc6cc929b0c2dfedd4d0327c8ef8654
Seven Li <vinson7lhl@126.com>
2021-11-25 16:55:15 +0800

    修改axios封装

--- vue3_app/src/api/index-page-api/index_apge_api.js
+++ vue3_app/src/api/index-page-api/index_apge_api.js
@@ -4,8 +4,14 @@ const INDEX_PAGE_API = {
 	/**
 	 * @description 获取用户列表
 	 */
-	get_users: (url, params) => {
-		return BASE_API.GET(url, params)
+	get_users: (params) => {
+		return BASE_API.GET('真实url地址', params)
+	},
+	/**
+	 * @description 获取用户列表
+	 */
+	 post_form: (data) => {
+		return BASE_API.POST('真实url地址', data)
 	}
 }
 export default INDEX_PAGE_API
