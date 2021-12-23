940138e071040ae00e66ff24f260cca0e56e9890
Seven Li-后会有七 <vinson7lhl@126.com>
2021-12-23 19:53:13 +0800

    提交

--- base_vue3/src/App.vue
+++ base_vue3/src/App.vue
@@ -1,39 +1,48 @@
 <template>
     <div id="nav">
-        <router-link to="/">Home</router-link> |
-        <router-link to="/about">About</router-link>
-        <el-row>
-            <el-button>Default</el-button>
-            <el-button type="primary">Primary</el-button>
-            <el-button type="success">Success</el-button>
-            <el-button type="info">Info</el-button>
-            <el-button type="warning">Warning</el-button>
-            <el-button type="danger">Danger</el-button>
-            <el-button>中文</el-button>
-        </el-row>
+        <el-config-provider :locale="locale">
+            <router-link to="/">Home</router-link> |
+            <router-link to="/about">About</router-link>
+            <el-row>
+                <el-button>Default</el-button>
+                <el-button type="primary">Primary</el-button>
+                <el-button type="success">Success</el-button>
+                <el-button type="info">Info</el-button>
+                <el-button type="warning">Warning</el-button>
+                <el-button type="danger">Danger</el-button>
+                <el-button>中文</el-button>
+            </el-row>
 
-        <el-row>
-            <el-button :icon="Search" circle></el-button>
-            <el-button type="primary" :icon="Edit" circle></el-button>
-            <el-button type="success" :icon="Check" circle></el-button>
-            <el-button type="info" :icon="Message" circle></el-button>
-            <el-button type="warning" :icon="Star" circle></el-button>
-            <el-button type="danger" :icon="Delete" circle></el-button>
-        </el-row>
-        <el-icon :size="20" color="#409EFC">
-            <edit />
-        </el-icon>
-        <div>测试</div>
+            <el-row>
+                <el-button :icon="Search" circle></el-button>
+                <el-button type="primary" :icon="Edit" circle></el-button>
+                <el-button type="success" :icon="Check" circle></el-button>
+                <el-button type="info" :icon="Message" circle></el-button>
+                <el-button type="warning" :icon="Star" circle></el-button>
+                <el-button type="danger" :icon="Delete" circle></el-button>
+            </el-row>
+            <el-icon :size="20" color="#409EFC">
+                <edit />
+            </el-icon>
+            <div>测试</div>
+            <el-table :data="tableData" style="width: 100%" :loading='loading'>
+                <el-table-column prop="date" label="Date" width="180" />
+                <el-table-column prop="name" label="Name" width="180" />
+                <el-table-column prop="address" label="Address" />
+            </el-table>
+            <router-view />
+        </el-config-provider>
     </div>
-    <router-view />
 </template>
 
 <script>
 import { Search, Edit, Check, Message, Star, Delete } from '@element-plus/icons'
+import { ElConfigProvider } from 'element-plus'
+import zhCn from 'element-plus/lib/locale/lang/zh-cn'
 
 export default {
     name: 'Home',
-    components: { Edit },
+    components: { Edit, ElConfigProvider },
     data() {
         return {
             Search,
@@ -42,6 +51,30 @@ export default {
             Message,
             Star,
             Delete,
+            locale: zhCn,
+            loading: true,
+            tableData: [
+                {
+                date: '2016-05-03',
+                name: 'Tom',
+                address: 'No. 189, Grove St, Los Angeles',
+                },
+                {
+                date: '2016-05-02',
+                name: 'Tom',
+                address: 'No. 189, Grove St, Los Angeles',
+                },
+                {
+                date: '2016-05-04',
+                name: 'Tom',
+                address: 'No. 189, Grove St, Los Angeles',
+                },
+                {
+                date: '2016-05-01',
+                name: 'Tom',
+                address: 'No. 189, Grove St, Los Angeles',
+                },
+            ]
         }
     },
 }
